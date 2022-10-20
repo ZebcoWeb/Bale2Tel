48b07239a922d9cc6d50d1bff5e5054de40b52ef
ZebcoWeb <zebco1382@gmail.com>
2022-10-20 21:53:57 +0330

    Added some logging

--- run.py
+++ run.py
@@ -6,7 +6,11 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters
 from telegram.constants import ParseMode
 from telegram import Bot, Update
 
+__version__ = "1.0"
+
+
 load_dotenv('.env')
+
 
 BALE_TOKEN = os.getenv('BALE_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -40,41 +44,46 @@ async def bale_to_telegram(update: Update, context):
     if update.message:
         channel = update.message.chat.username
         if channel in list(CHANNEL_ROUTES.keys()):
-            if update.message.video:
-                video = await update.message.video.get_file()
-                buffer = await video.download_as_bytearray()
+            try:
+                if update.message.video:
+                    video = await update.message.video.get_file()
+                    buffer = await video.download_as_bytearray()
 
-                await tel_bot.send_video(
-                    CHANNEL_ROUTES[channel],
-                    video=bytes(buffer),
-                    caption=update.message.caption,
-                    supports_streaming=True,
-                )
-            elif update.message.photo:
-                photo = await update.message.photo[0].get_file()
-                buffer = await photo.download_as_bytearray()
-
-                await tel_bot.send_photo(
-                    CHANNEL_ROUTES[channel],
-                    photo=bytes(buffer),
-                    caption=update.message.caption,
-                )
-            elif update.message.document:
-                document = await update.message.document.get_file()
-                buffer = await document.download_as_bytearray()
-
-                await tel_bot.send_document(
-                    CHANNEL_ROUTES[channel],
-                    document=bytes(buffer),
-                    caption=update.message.caption,
-                )
-            else:
-                if update.message.text:
-                    await tel_bot.send_message(
+                    await tel_bot.send_video(
                         CHANNEL_ROUTES[channel],
-                        text=update.message.text,
-                        parse_mode=ParseMode.MARKDOWN,
+                        video=bytes(buffer),
+                        caption=update.message.caption,
+                        supports_streaming=True,
                     )
+                elif update.message.photo:
+                    photo = await update.message.photo[0].get_file()
+                    buffer = await photo.download_as_bytearray()
+
+                    await tel_bot.send_photo(
+                        CHANNEL_ROUTES[channel],
+                        photo=bytes(buffer),
+                        caption=update.message.caption,
+                    )
+                elif update.message.document:
+                    document = await update.message.document.get_file()
+                    buffer = await document.download_as_bytearray()
+
+                    await tel_bot.send_document(
+                        CHANNEL_ROUTES[channel],
+                        document=bytes(buffer),
+                        caption=update.message.caption,
+                    )
+                else:
+                    if update.message.text:
+                        await tel_bot.send_message(
+                            CHANNEL_ROUTES[channel],
+                            text=update.message.text,
+                            parse_mode=ParseMode.MARKDOWN,
+                        )
+            except Exception as e:
+                logger.error(e)
+        else:
+            logger.warning(f'Channel {channel} not found in channel_routes.json')
 
 
 bale_app.add_handler(
@@ -84,4 +93,7 @@ bale_app.add_handler(
     )
 )
 
-bale_app.run_polling()
+
+if __name__ == '__main__':
+    logging.info(f"Starting Bale2Tel Bot v{__version__}")
+    bale_app.run_polling()
